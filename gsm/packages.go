6e32440241e2672ad54e6de43c4a0b61c02bfcb0
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-22 09:16:42 -0700

    use dir options on pcfg

--- gsm/packages.go
+++ gsm/packages.go
@@ -17,34 +17,21 @@ import (
 // in the current directory (assumed to be the GoKi root directory).
 func ParsePackages() ([]*packages.Package, error) {
 	res := []*packages.Package{}
-	pcfg := &packages.Config{
-		Mode:  PackageModes(),
-		Tests: false,
-	}
-	// need to get separately because [os.DirFS] on a relative path changes when we call [os.Chdir] later
-	cdir, err := os.Getwd()
-	if err != nil {
-		return nil, fmt.Errorf("error getting current working directory: %w", err)
-	}
-	err = fs.WalkDir(os.DirFS(cdir), ".", func(path string, d fs.DirEntry, perr error) error {
+	err := fs.WalkDir(os.DirFS("."), ".", func(path string, d fs.DirEntry, perr error) error {
 		if d.Name() != "go.mod" {
 			return nil
 		}
 		dir := filepath.Dir(path)
-		// need to use [os.Chdir] because you must be in the same module for [packages.Load] to work
-		err := os.Chdir(dir)
-		if err != nil {
-			return fmt.Errorf("error changing to module directory %q: %w", dir, err)
+		pcfg := &packages.Config{
+			Mode:  PackageModes(),
+			Dir:   dir,
+			Tests: false,
 		}
 		pkgs, err := packages.Load(pcfg, ".")
 		if err != nil {
 			return fmt.Errorf("error loading package %q: %w", dir, err)
 		}
 		res = append(res, pkgs...)
-		err = os.Chdir(cdir)
-		if err != nil {
-			return fmt.Errorf("error changing back to base directory %q: %w", cdir, err)
-		}
 		return nil
 	})
 	return res, err
