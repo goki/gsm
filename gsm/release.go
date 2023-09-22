2c12c65b951fd4769e5c516cba086c9b3ad7f569
Kai O'Reilly <koreilly5297@gmail.com>
2023-09-21 20:48:58 -0700

    moved clone, added release, other improvements

---
+++ gsm/release.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gsm
+
+// Release releases all of the GoKi Go repositories in the current folder,
+// recursively updating each one and all of its dependencies, but stopping
+// after a couple of iterations due to pseudo-import cycles at the module level.
+//
+//gti:add
+func Release(c *Config) error {
+	return nil
+}
--- gsm/repository.go
+++ gsm/repository.go
@@ -5,52 +5,13 @@
 package gsm
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
-	"os"
 	"path"
-	"sync"
 
-	"goki.dev/xe"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
-
-// Clone concurrently clones all of the GoKi Go repositories into the current directory.
-// It does not clone repositories that the user already has in the current directory.
-//
-//gti:add
-func Clone(c *Config) error {
-	reps, err := GetRepositories()
-	if err != nil {
-		return fmt.Errorf("error getting repositories: %w", err)
-	}
-	wg := sync.WaitGroup{}
-	wg.Add(len(reps))
-	var errs []error
-	for _, rep := range reps {
-		rep := rep
-		go func() {
-			defer wg.Done()
-			fi, err := os.Stat(rep.Name)
-			if err == nil { // no error means it already exists
-				if fi.IsDir() { // if we already have dir, we don't need to clone
-					return
-				} else {
-					errs = append(errs, fmt.Errorf("file %q (for repository %q) already exists and is not a directory", rep.Name, rep.Title))
-					return
-				}
-			}
-			err = xe.Run(xe.VerboseConfig(), "git", "clone", rep.RepositoryURL)
-			if err != nil {
-				errs = append(errs, fmt.Errorf("error cloning repository: %w", err))
-			}
-		}()
-	}
-	wg.Wait()
-	return errors.Join(errs...)
-}
 
 // Repository represents a GoKi Go repository
 type Repository struct {
